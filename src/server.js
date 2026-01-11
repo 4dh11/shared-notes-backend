ea32284b5f2db4bccd31bce5239d4c661e68e778
Adityaa SS <adityaa.sureshbabu@gmail.com>
2026-01-11 21:51:01 +0530

    Implement ping endpoint
    Add '/ping' endpoint that responds with 'pong'

--- src/server.js
+++ src/server.js
@@ -51,3 +51,8 @@ app.use("/uploads", express.static(path.join(__dirname, "../uploads")))
 app.listen(PORT, () => {
   console.log("Server started on PORT:", PORT)
 })
+
+// This tells your server to respond with "pong" when it hears "ping"
+app.get('/ping', (req, res) => {
+  res.status(200).send('pong');
+});
